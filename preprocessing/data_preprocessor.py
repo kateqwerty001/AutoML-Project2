bd268d17fd564934f71eeb99232f73028aa74d96
rzewnickad <126004325+rzewnickad@users.noreply.github.com>
2025-01-04 22:52:23 +0100

    update: handled unbalanced data and correlated columns

--- preprocessing/data_preprocessor.py
+++ preprocessing/data_preprocessor.py
@@ -2,15 +2,19 @@ from .redundant_features_handler import RedundantFeaturesHandler
 from .missing_values_handler import MissingValuesHandler
 from .outliers_handler import OutliersHandler
 from .feature_type_extractor import FeatureTypeExtractor
+from .correlated_features_handler import CorrelationFeaturesHandler
+from .class_balance_handler import ClassBalanceHandler
 import warnings
+
 
 class DataPreprocessor:
     '''
-    A class where 
+    A class where
     '''
+
     def __init__(self, dataset, target_column_name):
-        self.dataset = dataset.copy() # the whole dataset, with a target column
-        self.target_column_name = target_column_name # the name of the target column
+        self.dataset = dataset.copy()  # the whole dataset, with a target column
+        self.target_column_name = target_column_name  # the name of the target column
 
     def preprocess(self):
         '''
@@ -23,7 +27,7 @@ class DataPreprocessor:
 
         print('---------------Extracting Day, Month and Year--------------')
         self.dataset = FeatureTypeExtractor().separate_datetime(self.dataset)
-        
+
         print('---------------Deleting redundant features--------------')
         self.dataset = RedundantFeaturesHandler(self.dataset).handle()
 
@@ -42,7 +46,15 @@ class DataPreprocessor:
         print('---------------3. Encoding of target------------')
         self.dataset = FeatureTypeExtractor().encode_target(self.dataset, self.target_column_name)
 
-        print('---------------Dataset preprocessing is done------------')
         X = self.dataset.drop(self.target_column_name, axis=1)
         y = self.dataset[self.target_column_name]
+
+        print('---------------Removing highly correlated columns ------------')
+        X = CorrelationFeaturesHandler().fit_transform(X)
+
+        print('---------------Handling imbalanced classes------------')
+        X, y = ClassBalanceHandler().fit_resample(X, y)
+
+        print('---------------Dataset preprocessing is done------------')
+
         return X, y
